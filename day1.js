6ecd96eb0ad471edfa540d95cd0344bb8fdd841b
AlanBM <black.computer.things@gmail.com>
2020-09-28 11:28:30 -0500

    day1

---
+++ day1.js
@@ -0,0 +1,24 @@
+document.getElementById("button").onclick =function(){
+	document.getElementById("confirm").innerHTML ="Order placed. Check email for confirmation";
+	document.getElementById("button").style.display="none";
+
+	var username = prompt();
+
+alert(username)
+
+//Comment
+/*
+
+Another comment
+*/
+
+
+
+
+
+
+
+
+
+	alert("Hello world")
+}
