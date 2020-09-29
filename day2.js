8c824f2e3fefbeb13a73d1d50a60803f9e6d7e37
AlanBM <black.computer.things@gmail.com>
2020-09-29 09:18:37 -0500

    Day2

---
+++ day2.js
@@ -0,0 +1,9 @@
+//how to create a function
+alert('dominó');
+var greet = function(){
+	//IIFE - immediatly invoked function expression.
+	var age =9;
+
+}();
+
+//There is a thing called window object that is important.
